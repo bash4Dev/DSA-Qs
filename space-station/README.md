d3c4249ac91768fb907fc6c3d5587790780ae22f
Bashir Umar Idris <bashtrust15@gmail.com>
2024-08-21 15:58:10 +0100

    Commit second task

--- space-station/README.md
+++ space-station/README.md
@@ -1,5 +1,7 @@
 # Space Exploration - Planet Analysis
 
-## Task
+## Tasks
 
-- Make a planet analyzer which will determine if a planet with mid-range temperatures is "Moderate" and suitable for a space station. Use a nested ternary operator to add this new condition.
+1. Make a planet analyzer which will determine if a planet with mid-range temperatures is "Moderate" and suitable for a space station. Use a nested ternary operator to add this new condition.
+
+2. Add the appropriate code to determine whether the planet has moons and display the message.
--- space-station/solution.js
+++ space-station/solution.js
@@ -1,19 +1,33 @@
+// Task 1
 // We're analyzing if a planet is suitable for a space station.
 let planet = {
     hasWater: true,
     hasAtmosphere: true,
-    temperature: 15  // Temperature is now in the moderate range
-  };
+    temperature: 15  // Temperature in the moderate range
+};
+
+let analysisResult = planet.hasWater
+  ? (planet.hasAtmosphere
+    ? (planet.temperature > 0
+      ? (planet.temperature < 30
+        ? 'Perfect for a space station!'
+        : 'Temperature not suitable.')
+      : 'Temperature too low.')
+    : 'Lacks atmosphere.')
+  : 'Unsuitable, no water detected.';
+
+document.getElementById('planetAnalysis').textContent = analysisResult;
+
+
+//Task 2
+// Let's figure out which planets have moons
+let planets = [
+  { name: 'Mercury', hasMoons: false },
+  { name: 'Venus', hasMoons: false },
+  { name: 'Earth', hasMoons: true },
+];
   
-  // Apply a nested ternary operator based on the new planet properties.
-  let analysisResult = planet.hasWater
-    ? (planet.hasAtmosphere
-      ? (planet.temperature > 0
-        ? (planet.temperature < 30
-          ? 'Perfect for a space station!'
-          : 'Temperature not suitable.')
-        : 'Temperature too low.')
-      : 'Lacks atmosphere.')
-    : 'Unsuitable, no water detected.';
-  
-  document.getElementById('planetAnalysis').textContent = analysisResult;
+planets.forEach((planet) => {
+  let moonMessage = planet.hasMoons ? `${planet.name} has moons` : `${planet.name} has no moons`;
+  document.getElementById('cosmicInfo').textContent += moonMessage + " ";
+});
