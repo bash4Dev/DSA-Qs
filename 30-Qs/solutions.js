517de32ea753cd32dd56249cc9a67a3c58ab6a16
Bashir Umar Idris <bashtrust15@gmail.com>
2024-09-04 13:30:15 +0100

    commit
    Solve 2 questions

---
+++ 30-Qs/solutions.js
@@ -0,0 +1,16 @@
+// Q1 - Write a function that takes an array of integers and returns the maximum number.
+const maxNumb = (array) => {
+    console.log(`Integers: ${array}`);
+    let maxVal = Math.max(...array);
+    return console.log(`Max int: ${maxVal}`);
+}
+console.log(maxNumb([20, 13, 27, 6, 18]));
+
+// Q2 - Write a function that checks if a given string is a palindrome (ignoring spaces, punctuation, and capitalization).
+const checkPalindrome = (input) => {
+    const palRegex = input.toLowerCase().replace(/([\s])/gi, '');
+    const reversed = input.split('').reverse().join('');
+    return (palRegex === reversed) ? 'Is a palindrome' : 'Not a palindrome';
+}
+console.log(checkPalindrome('eye'));
+console.log(checkPalindrome('nope'));
