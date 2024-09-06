d60a421cefbf39f85b973739fb1002bbf4231c3b
Bashir Umar Idris <bashtrust15@gmail.com>
2024-09-06 22:11:17 +0100

    Add another Q

--- 30-Qs/solutions.js
+++ 30-Qs/solutions.js
@@ -1,4 +1,4 @@
-/* Q1
+/** Q1
  * Write a function that takes an array of integers 
  * and returns the maximum number.
  */
@@ -9,13 +9,13 @@ const maxNumb = (array) => {
 }
 console.log(maxNumb([20, 13, 27, 6, 18]));
 
-/* Q2
+/** Q2
  * Write a function to reverse a string.
  */
 const reverseStr = (str) => str.split('').reverse().join('');
 reverseStr('bashir');
 
-/* Q3
+/** Q3
  * Write a function that checks if a given string is a palindrome
  * (ignoring spaces, punctuation, and capitalization).
  */
@@ -27,9 +27,9 @@ const checkPalindrome = (input) => {
 console.log(checkPalindrome('eye'));
 console.log(checkPalindrome('nope'));
 
-/* Q4
- * Given a sorted array, remove the duplicates in-place such that each element appears only once 
- * and return the new length.
+/** Q4
+ * Given a sorted array, remove the duplicates in-place such that each element appears only once
+ * and return the new length. 
  */
 function compare(a, b) {
     return a - b;
@@ -37,5 +37,13 @@ function compare(a, b) {
 let array = [2, 20, 5, 8, 15, 3, 2, 8];
 let sortedArr = array.sort(compare);
 
-for (arr of sortedArr) {
-}
+// for (arr of sortedArr) {
+// }
+
+/** Q5
+ * Given two sorted arrays, merge them into one sorted array.
+ */
+let array2 = [3,5,8,14,1,23,41,3];
+
+let sortedArr2 = array2.sort(compare);
+let mergedArr = sortedArr + sortedArr2;
